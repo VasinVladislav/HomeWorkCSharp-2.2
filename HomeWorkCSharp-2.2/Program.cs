12eb4c790e234bb0270b8b8586e84a1a01bd88d6
Владислав Васин <VasinVladislav94@gmail.com>
2024-11-27 14:30:01 +0300

    Добавьте файлы проекта.

---
+++ HomeWorkCSharp-2.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkCSharp_2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //2.2. Дана сторона квадрата. Найти его периметр.
+            Console.WriteLine("Найдите периметр квадрата");
+        input:
+            Console.Write("Сторона квадрата равна: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            while (!(a >= 0))
+            {
+                Console.WriteLine("Вы ввели неверное значение");
+                goto input;
+            }
+            double P = 4 * a;
+            Console.WriteLine("Периметр квадрата равен " + P);
+        }
+    }
+}
